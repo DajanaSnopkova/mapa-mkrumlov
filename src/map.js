e5ddedb4ac7ec3652583104e8e92995325ba24e9
DajanaSnopkova <dajana.snopkova@geogr.muni.cz>
2025-04-14 14:50:02 +0200

    pridani vrstev opatreni

--- src/map.js
+++ src/map.js
@@ -29,7 +29,7 @@ function loadGeoJSON(url, layer) {
         .catch(error => console.error(`Error loading ${url}:`, error));
 }
 
-// funkce pro načtení barvy parcely podle druhu pozemku
+// funkce pro načtení barvy parcely podle atributu
 function getColor(property) {
     return property == 2 ? '#cc7d0f' :
            property == 4 ? '#c3e332' :
@@ -41,8 +41,30 @@ function getColor(property) {
            property == 13 ? '#a5aaa8' :
            property == 14 ? '#d0b7c7' :
                               '#ffffff';
-  }
+};
 
+function getColorNavrzenaOpatreni(property) {
+    return property == 'BC' ? '#cc7d0f' :
+           property == 'BK' ? '#c3e332' :
+           property == 'DR' ? '#9fe8ba' :
+           property == 'PC' ? '#89eae8' :
+           property == 'RL' ? '#90ce45' :
+           property == 'SM' ? '#068246' :
+           property == 'VL' ? '#66a3c9' :
+                            '#ffffff';
+};
+
+function getColorErozniOpatreni(property) {
+    return property == 'H' ? '#cc7d0f' :
+    property == 'P' ? '#c3e332' :
+    property == 'S' ? '#9fe8ba' :
+    property == 'T' ? '#89eae8' :
+    property == 'V' ? '#90ce45' :
+    property == 'Z' ? '#66a3c9' :
+                     '#ffffff';
+};
+
+/*
 // Vytvoření geoJSON vrstvy pro parcely města Krumlov
 var parcelyK = L.geoJSON(null, {
     style: function (feature) {
@@ -70,11 +92,11 @@ var parcelyK = L.geoJSON(null, {
             parcelyK.resetStyle(e.target);
         });
     }
-}).addTo(map);
+});*/
 
 // Vytvoření vrstev parcel vlastnictví
 function createParcelLayer(fillColor, hoverColor = '#261a20') {
-    return L.geoJSON(null, {
+    const geoJsonLayer = L.geoJSON(null, {
         style: function (feature) {
             return {
                 fillColor: fillColor,
@@ -83,8 +105,25 @@ function createParcelLayer(fillColor, hoverColor = '#261a20') {
                 fillOpacity: 0.2
             };
         },
+        onEachFeature: function (feature, layer) {
+            layer.on('mouseover', function (e) {
+                layer.bindTooltip("<b>Číslo parcely: </b> " + feature.properties.KmenoveCislo + (feature.properties.PododdeleniCisla ? "/" + feature.properties.PododdeleniCisla : "")).openTooltip();
+                e.target.setStyle({
+                    weight: 3,
+                    color: hoverColor
+                });
+                e.target.bringToFront();
+            });
+
+            layer.on('mouseout', function (e) {
+                geoJsonLayer.resetStyle(e.target);  // This resets it to the default defined in `style:`
+            });
+        }
     });
+
+    return geoJsonLayer;
 };
+
 
 // Vytvoření geoJSON vrstvy pro parcely podle druhu pozemku (bez dat)
 var parcelyDP = L.geoJSON(null, {
@@ -114,8 +153,33 @@ var parcelyDP = L.geoJSON(null, {
     }
 });
 
-loadGeoJSON('https://raw.githubusercontent.com/DajanaSnopkova/mapa-mkrumlov/main/data/parcely_Krumlov.geojson', parcelyK);
+// Vytvoření geoJSON vrstvy pro parcely podle navržených opatření (bez dat)
+var parcelyNavrhOpatreni = L.geoJSON(null, {
+    style: function (feature) {
+        return {
+            fillColor: getColorNavrzenaOpatreni(feature.properties["Návrh opatření"]),
+            color: getColorNavrzenaOpatreni(feature.properties["Návrh opatření"]),
+            weight: 0.6,
+            fillOpacity: 0.6,
+        };
+    }
+}).addTo(map);
+
+// Vytvoření geoJSON vrstvy pro parcely podle protierozních opatření (bez dat)
+var parcelyErozniOpatreni = L.geoJSON(null, {
+    style: function (feature) {
+        return {
+            fillColor: getColorErozniOpatreni(feature.properties["Protierozní opatření"]),
+            color: getColorErozniOpatreni(feature.properties["Protierozní opatření"]),
+            weight: 0.6,
+            fillOpacity: 0.6,
+        };
+    }
+});
+
 loadGeoJSON('https://raw.githubusercontent.com/DajanaSnopkova/mapa-mkrumlov/main/data/parcely_Krumlov.geojson', parcelyDP);
+loadGeoJSON('https://raw.githubusercontent.com/DajanaSnopkova/mapa-mkrumlov/main/data/navrhOpatreni.geojson', parcelyNavrhOpatreni);
+loadGeoJSON('https://raw.githubusercontent.com/DajanaSnopkova/mapa-mkrumlov/main/data/navrhOpatreni.geojson', parcelyErozniOpatreni);
 
 // Přepínač vrstev
 var baseMaps = {
@@ -124,29 +188,35 @@ var baseMaps = {
 };
 
 var overlayMaps = {
+    "Navržená opatření": parcelyNavrhOpatreni,
+    "Protierozní opatření": parcelyErozniOpatreni,
     "Parcely podle druhu pozemku": parcelyDP,
-    "Parcely města M. Krumlov": parcelyK,
+    //"Parcely města M. Krumlov": parcelyK,
     //"Katastrální mapa": kmGridLayer,
 };
 
 // Parcely podle vlastnictví zdroj a styl
 const vlastnictviLayers = [
+    { name: "Parcely města M. Krumlov", file: "parcely_mesta.geojson", color: "#0AFFF5" },
     { name: "Parcely vlastněné státem", file: "parcely_CR.geojson", color: "#ff7184" },
     { name: "Parcely vlastníka AGRO 2000 s.r.o.", file: "parcely_Agro.geojson", color: "#dcff71" },
     { name: "Parcely vlastníka AGRO Rakšice, spol. s r. o.", file: "parcely_AgroRaksice.geojson", color: "#61e82a" },
     { name: "Parcely vlastníka Jiří Břínek", file: "parcely_BrinekJiri.geojson", color: "#c971ff" },
     { name: "Parcely vlastníka Vratislav Břínek", file: "parcely_BrinekVratislav.geojson", color: "#e5a8d9" },
-    { name: "Parcely vlastníka BioZem s.r.o.", file: "parcely_BioZem.geojson", color: "#ffe171" }
+    { name: "Parcely vlastníka BioZem s.r.o.", file: "parcely_BioZem.geojson", color: "#ffe171" },
 ];
 
 vlastnictviLayers.forEach(item => {
     let layer = createParcelLayer(item.color);
     overlayMaps[item.name] = layer;
     loadGeoJSON(`https://raw.githubusercontent.com/DajanaSnopkova/mapa-mkrumlov/main/data/${item.file}`, layer);
+    
+    layer.on('add', toggleOwnerLegend);
+    layer.on('remove', toggleOwnerLegend);
 });
 
 
-//Legenda
+//Legenda druh pozemku
 var legend = L.control({position: 'bottomright'});
 legend.onAdd = function (map) {
     var div = L.DomUtil.create('div', 'info legend'),
@@ -161,6 +231,38 @@ legend.onAdd = function (map) {
     return div;
 };
 
+//Legenda navržená opatření
+var legendNavrhOpatreni = L.control({position: 'bottomright'});
+legendNavrhOpatreni.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        druhyNO = ['BC', 'BK', 'DR', 'PC', 'RL', 'SM', 'VL'];
+        druhyNOlabels = ['biocentrum', 'biokoridor', 'ponechat', 'polní cesta', 'rozvojová plocha', 'směna pozemků', 'větrolam'];
+    div.innerHTML += '<h4>Navržená opatření</h4>';
+    for (var i = 0; i < druhyNO.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColorNavrzenaOpatreni(druhyNO[i]) + '"></i> ' +
+            (druhyNOlabels[i]) + '<br>';
+    }
+    return div;
+};
+
+legendNavrhOpatreni.addTo(map);
+
+//Legenda protierozní opatření	
+var legendErozniOpatreni = L.control({position: 'bottomright'});
+legendErozniOpatreni.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        druhyEO = ['H', 'P', 'S', 'T', 'V', 'Z'];
+        druhyEOlabels = ['úprava terénu (příkopy, terasy)', 'úprava vodoteče (přehrážky, stabilizace)', 'svejl (zasakovací prohlubně s výsadbou)', 'tůň, mokřad', 'výsadby dřevin, solitérů, stromořadí', 'zachování stavu, zatravnění'];
+    div.innerHTML += '<h4>Protierozní opatření</h4>';
+    for (var i = 0; i < druhyEO.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColorErozniOpatreni(druhyEO[i]) + '"></i> ' +
+            (druhyEOlabels[i]) + '<br>';
+    }
+    return div;
+};
+
 //Legenda vlastnictví
 var legendOwners = L.control({position: 'bottomright'});
 legendOwners.onAdd = function (map) {
@@ -168,7 +270,6 @@ legendOwners.onAdd = function (map) {
         colors = vlastnictviLayers.map(item => item.color);
         labels = vlastnictviLayers.map(item => item.name);
     div.innerHTML += '<h4>Parcely podle vlastnictví</h4>';
-    div.innerHTML += '<i style="background:#0AFFF5"></i> Parcely města M. Krumlov<br>';
     for (var i = 0; i < colors.length; i++) {
         div.innerHTML +=
             '<i style="background:' + colors[i] + '"></i> ' +
@@ -177,19 +278,39 @@ legendOwners.onAdd = function (map) {
     return div;
 };
 
-legendOwners.addTo(map);
-
-// Zobrazit legendu, pokud je aktivní vrstva parcelyDP
-function toggleLegend() {
-    if (map.hasLayer(parcelyDP)) {
-        legend.addTo(map);
+// Zobrazit legendu, pokud je aktivní vrstva
+function toggleLegend(layerName, legendName) {
+    if (map.hasLayer(layerName)) {
+        legendName.addTo(map);
     } else {
-        map.removeControl(legend);
+        map.removeControl(legendName);
     }
-}
+};
+
+// Zobrazit legendu, pokud je aktivní vrstva podle vlastnictví
+function anyOwnerLayerVisible() {
+    return vlastnictviLayers.some(item => {
+        let layer = overlayMaps[item.name];
+        return map.hasLayer(layer);
+    });
+};
+
+function toggleOwnerLegend() {
+    if (anyOwnerLayerVisible()) {
+        legendOwners.addTo(map);
+    } else {
+        map.removeControl(legendOwners);
+    }
+};
 
 // Přidání legendy do mapy
-parcelyDP.on('add', toggleLegend);
-parcelyDP.on('remove', toggleLegend);
+parcelyDP.on('add', function () { toggleLegend(parcelyDP, legend); });
+parcelyDP.on('remove', function () { toggleLegend(parcelyDP, legend); });
+
+parcelyNavrhOpatreni.on('add', function () { toggleLegend(parcelyNavrhOpatreni, legendNavrhOpatreni); });
+parcelyNavrhOpatreni.on('remove', function () { toggleLegend(parcelyNavrhOpatreni, legendNavrhOpatreni); });
+
+parcelyErozniOpatreni.on('add', function () { toggleLegend(parcelyErozniOpatreni, legendErozniOpatreni); });
+parcelyErozniOpatreni.on('remove', function () { toggleLegend(parcelyErozniOpatreni, legendErozniOpatreni); });
 
 L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(map);
